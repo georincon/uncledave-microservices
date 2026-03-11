76d5e9eae0e91d3533e81ac459fdddfe23d376c9
georincon <georincon@gmail.com>
2026-03-11 16:57:32 -0500

    api-gateway

--- orders-service/src/main/java/com/uncledavecode/orders_service/services/OrderService.java
+++ orders-service/src/main/java/com/uncledavecode/orders_service/services/OrderService.java
@@ -1,8 +1,6 @@
 package com.uncledavecode.orders_service.services;
 
-import com.uncledavecode.orders_service.model.dtos.BaseResponse;
-import com.uncledavecode.orders_service.model.dtos.OrderItemRequest;
-import com.uncledavecode.orders_service.model.dtos.OrderRequest;
+import com.uncledavecode.orders_service.model.dtos.*;
 import com.uncledavecode.orders_service.model.entities.Order;
 import com.uncledavecode.orders_service.model.entities.OrderItems;
 import com.uncledavecode.orders_service.repositories.OrderRepository;
@@ -10,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -42,6 +41,21 @@ public class OrderService {
             throw new IllegalArgumentException("Some of the products are not in stock");
         }
    }
+
+   public List<OrderResponse> getAllOrders() {
+        List<Order> orders = this.orderRepository.findAll();
+
+        return orders.stream().map(this::mapToOrderResponse).toList();
+    }
+
+    private OrderResponse mapToOrderResponse(Order order) {
+        return new OrderResponse(order.getId(), order.getOrderNumber()
+                , order.getOrderItems().stream().map(this::mapToOrderItemRequest).toList());
+    }
+
+    private OrderItemsResponse mapToOrderItemRequest(OrderItems orderItems) {
+        return new OrderItemsResponse(orderItems.getId(), orderItems.getSku(), orderItems.getPrice(), orderItems.getQuantity());
+    }
 
     private OrderItems mapOrderItemRequestToOrderItem(OrderItemRequest orderItemRequest, Order order) {
 
